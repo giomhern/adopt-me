c4a65e77281515c19f24f289c3386454e3c4d741
giovannimhern <giovannimhern@uchicago.edu>
2024-03-31 00:41:20 -0500

    Creating initial filtering capabilities

---
+++ src/SearchParams.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return <option key={animal}>{animal}</option>;
+            })}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => {
+              return <option key={breed}>{breed}</option>;
+            })}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
