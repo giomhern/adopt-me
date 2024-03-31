311e4b55b15b8644c8e83fd4709b1dcde04fad18
giovannimhern <giovannimhern@uchicago.edu>
2024-03-31 00:04:29 -0500

    More eslint config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Dog" breed="Mixed" />
+    </div>
+  );
+};
+export default App;
