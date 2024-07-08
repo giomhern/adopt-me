dfa7d7513849e1f011abb61fdf2440cf0400d5ac
giovannimhern <giovannimhern@uchicago.edu>
2024-07-08 14:23:35 -0700

    Implementation: boilerplate of carousel

---
+++ src/Carousel.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+// class components do not mix with the hooks
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Carousel;
